ee2068789bdb489a04927c9b1449aa9e74e58a31
Yassir <james.iharo@gmail.com>
2023-09-19 15:09:35 +0100

    orgonazing my files

---
+++ L_C/f_0.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+void swap(int a, int b)
+{
+    int t = a;
+    a = b;
+    b = t;
+    printf("swap: a = %d, b = %d\n", a, b);
+}
+
---
+++ L_C/main_0.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+    int a = 21;
+    int b = 17;
+
+    swap(a, b);
+    printf("main: a = %d, b = %d\n", a, b);
+    return (0);
+}
