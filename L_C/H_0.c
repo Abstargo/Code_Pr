f48fede672b74097ff7f7fc5926a455090379517
Yassir <james.iharo@gmail.com>
2023-09-23 16:11:34 +0100

    challenge0

---
+++ L_C/H_0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char *s = "Welcome to C programming.";
+    char *a = "Hello, World!";
+
+    printf("%s\n", a);
+    printf("%s", s);
+
+    return (0);
+}
