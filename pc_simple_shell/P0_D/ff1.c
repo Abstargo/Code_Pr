bfe5dbd945d316a12ffa30d73fbb47970c4e5a17
Yassir <james.iharo@gmail.com>
2023-10-09 11:26:28 +0100

    TS

---
+++ pc_simple_shell/P0_D/ff1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main ()
+{
+
+    char Name[] = "Alexndra Abramov";
+    char DOB[] = "July 14, 1975";
+    char Mobile[] = "99-9999999999";
+
+    printf("Name: %s\nDOB: %s\nMobile: %s", Name, DOB, Mobile);
+}
