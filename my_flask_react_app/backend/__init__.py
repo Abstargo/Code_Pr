e0eca416409dd108f71b4353ee561f00d79c8877
Abstargo <james.iharo@gmail.com>
2024-08-27 17:15:55 +0100

    here is

--- my_flask_react_app/backend/__init__.py
+++ my_flask_react_app/backend/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
+
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
@@ -9,6 +10,9 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://Yassir:Abstar_8-8@localhost/my_project_db'
     db.init_app(app)
+
+    from flask_cors import CORS
+    CORS(app)
 
     from .routes import auth_bp
     app.register_blueprint(auth_bp)
