35d18a2485ba4f8b7d9612f4efba433c46d84dac
Abstargo <james.iharo@gmail.com>
2025-01-07 14:45:11 +0100

    weight

---
+++ L_C++/EX/pra.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+// This  program computes the weight that you have on earth to your weight on another planet;
+
+int main() {
+  
+  double ear_weight, weight_dist;
+  int number;
+  std::cout << "What is your earth weight fatass\n";
+  std::cin >> ear_weight;
+  std::cout << "What number of the planet that you want to fight on ?\n";
+  std::cin >> number;
+
+
+  if(number == 1) {
+    weight_dist == ear_weight * 0.38;
+    std::cout << "your fatass weight on that planet is\n" << weight_dist << "\n";
+  }
+  else if(number == 2) {
+    weight_dist == ear_weight * 0.91;
+    std::cout << "your fatass weight on that planet is\n" << weight_dist << "\n";
+  }
+  else if(number == 3) {
+    weight_dist == ear_weight * 0.38;
+    std::cout << "your fatass weight on that planet is\n" << weight_dist << "\n";
+  }
+  else if(number == 4) {
+    weight_dist == ear_weight * 2.34;
+    std::cout << "your fatass weight on that planet is\n" << weight_dist << "\n";
+  }
+  else if(number == 5) {
+    weight_dist == ear_weight * 1.06;
+    std::cout << "your fatass weight on that planet is\n" << weight_dist << "\n";
+  }
+  else if(number == 6) {
+    weight_dist == ear_weight * 0.92;
+    std::cout << "your fatass weight on that planet is\n" << weight_dist << "\n";
+  }
+  else if(number == 7) {
+    weight_dist == ear_weight * 1.19;
+    std::cout << "your fatass weight on that planet is\n" << weight_dist << "\n";
+  }
+  
+}
