ce2966e8e6304aa633d5b08f682c8a9c0a7daf2d
Abstargo <james.iharo@gmail.com>
2025-05-13 12:47:15 +0100

    some

---
+++ JS/file.js
@@ -0,0 +1,22 @@
+
+export function taskFirst()
+{
+
+	const task = 'I prefer const when I can.';
+	return task;
+}
+
+export funtion getLast()
+{
+
+	return ' is okay';
+}
+
+export function taskNext()
+{
+
+	var combination = 'But sometimes let';
+	combination += getLast();
+
+	return combination;
+}
