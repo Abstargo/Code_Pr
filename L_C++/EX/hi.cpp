371e3ddaa98e1ae0b5195fefc45a3b839239bff4
Abstargo <james.iahro@gmail.com>
2025-09-18 17:00:57 +0100

    .

---
+++ L_C++/EX/hi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+	// cout outputs integers as decimal integers:
+	cout << 0xFF << endl;
+	cout << "Value of 27 = "<< hex << 27 << dec << 0x7FFF << "hexadecimal"
+	     << endl;
+	return 0;
+}
